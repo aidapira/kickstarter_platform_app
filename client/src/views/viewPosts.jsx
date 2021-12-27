4046e1814f6ced8415b879acc3fd30054fef905d
aidapira <aidapirah@gmail.com>
2021-12-27 11:01:14 -0800

    add cards to view posts page

--- client/src/views/viewPosts.jsx
+++ client/src/views/viewPosts.jsx
@@ -1,17 +1,45 @@
 import React from "react";
+import BusinessCard from "./card";
+import { Grid } from "@material-ui/core";
 
 class ViewPosts extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            businessName: [],
+            businessDescription: []
+        }
+    }
+
     render() { 
-        return <div
-        style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '90vh'
-        }}
-    >
-        <h1>View Posts</h1>
-    </div>;
+        return (
+            <Grid container style={styles.gridContainer} spacing={4}>
+                <Grid item xs={12} sm={4} md={3}>
+                    <BusinessCard />
+                </Grid>
+                <Grid item xs={12} sm={4} md={3}>
+                    <BusinessCard />
+                </Grid>
+                <Grid item xs={12} sm={4} md={3}>
+                    <BusinessCard />
+                </Grid>
+                <Grid item xs={12} sm={4} md={3}>
+                    <BusinessCard />
+                </Grid>
+                <Grid item xs={12} sm={4} md={3}>
+                    <BusinessCard />
+                </Grid>
+                <Grid item xs={12} sm={4} md={3}>
+                    <BusinessCard />
+                </Grid>
+            </Grid> 
+        )
+    }
+}
+
+const styles = {
+    gridContainer: {
+        padding: "40px 60px 20px 60px"
     }
 }
  
